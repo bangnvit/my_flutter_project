139be4696666ffd4a228ed55f1a658cfdde22bf8
Bang Nguyen Van <bang.nguyenvan@emeal.nttdata.com>
2026-03-31 18:01:53 +0700

    variables

--- lib/gradient_containter.dart
+++ lib/gradient_containter.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:my_flutter_project/styled_text.dart';
 
+var beginAlignment = Alignment.topCenter;
+var endAlignment = Alignment.bottomCenter;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -9,12 +12,12 @@ class GradientContainer extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            const Color.fromARGB(255, 22, 245, 178),
-            const Color.fromARGB(255, 26, 133, 233),
+          colors: const [
+            Color.fromARGB(255, 22, 245, 178),
+            Color.fromARGB(255, 26, 133, 233),
           ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
+          begin: beginAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(child: StyledText('My App')),
